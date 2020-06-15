0bbc85da0d6b690c5c86e67b5a1e7c7ea71c2f01
Your Name <you@example.com>
2020-06-15 17:32:05 +0530

    commit

---
+++ git.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(psych)
+#Welcome to the wonderful world of Tidyverse! It is the most powerful 
+#collection of R packages for preparing, wrangling and visualizing data. 
+# v ggplot2 3.3.0     v purrr   0.3.3
+# v tibble  3.0.0     v dplyr   0.8.5
+# v tidyr   1.0.2     v stringr 1.4.0
+# v readr   1.3.1     v forcats 0.5.0
+# -- Conflicts ------------------------------------------ tidyverse_conflicts() --
+#   x dplyr::filter() masks stats::filter()
+# x dplyr::lag()    masks stats::lag()
